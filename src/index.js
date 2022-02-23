5cd6cf592b2d0fd61d2550493c3f9d0ae17e5a4e
Jimmy <dabrowski.mac.1@gmail.com>
2022-02-23 14:55:46 +0100

    I finished js code and working on css

--- src/index.js
+++ src/index.js
@@ -8,4 +8,33 @@ import './style.css';
 const pageContent = document.getElementById('content');
 pageContent.append(...mainPage());
 const body = document.querySelector('.body');
-body.append(...contact());
+body.append(...home());
+
+const buttonHome = document.querySelector('.home');
+const buttonMenu = document.querySelector('.menu');
+const buttonContact = document.querySelector('.contact');
+
+buttonHome.addEventListener('click', () => {
+    buttonContact.classList.remove('active');
+    buttonMenu.classList.remove('active');
+    buttonHome.classList.add('active');
+    body.innerHTML = "";
+    body.append(...home());
+
+});
+
+buttonMenu.addEventListener('click', () => {
+    buttonHome.classList.remove('active');
+    buttonContact.classList.remove('active');
+    buttonMenu.classList.add('active');
+    body.innerHTML = "";
+    body.append(...menu());
+});
+
+buttonContact.addEventListener('click', () => {
+    buttonHome.classList.remove('active');
+    buttonMenu.classList.remove('active');
+    buttonContact.classList.add('active');
+    body.innerHTML = "";
+    body.append(...contact());
+});
